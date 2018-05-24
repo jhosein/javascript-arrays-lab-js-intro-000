fa91a522a52996178d3d446ffec4280385e056f6
Jonathan Hosein <jonathan.hosein@live.com>
2018-05-24 04:20:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+
+  var newKittens = kittens.push(name);
+  retiurn newKittens;
+}
